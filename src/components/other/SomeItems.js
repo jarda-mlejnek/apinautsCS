b0646fb58c33eb6021548f96ba1caed6d16d0410
Milan M <mmladek@csas.cz>
2018-12-03 12:17:56 +0100

    firebase - get data

--- src/components/other/SomeItems.js
+++ src/components/other/SomeItems.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SomeItems(props) {
     
     const items = props.items.map((item) => {
-        return <div className="list-item">{props.customer}</div>
+        return <div className="list-item">{item.title}</div>
     })
 
     return (
--- src/containers/OtherContainer.js
+++ src/containers/OtherContainer.js
@@ -2,8 +2,6 @@ import React, { Component, lazy, Suspense } from 'react'
 import PageSection from '../components/shared/PageSection'
 import SomeForm from '../components/other/SomeForm'
 import Spinner from '../components/shared/Spinner'
-import { FirebaseService } from '../services/FirebaseService'
-
 const SomeItems = lazy(() => import('../components/other/SomeItems'))
 
 export default class OtherContainer extends Component {
@@ -14,14 +12,23 @@ export default class OtherContainer extends Component {
     }
 
     state = {
-        items: []
+        items: [],
+        itemsLoading: false
     }
 
     componentDidMount() {
-        FirebaseService.getItems()
-            .then((response) => {
-                console.log(response)
-                this.setState({items: response})
+        const itemsRef = window.firebase.database().ref('items')
+            let result = []
+            itemsRef.on('value', (snapshot) => {
+                let items = snapshot.val()
+                for (let item in items) {
+                    result.push({
+                        id: item,
+                        title: items[item].customer,
+                        user: items[item].email
+                    })
+                }
+                this.setState({items: result})
             })
     }
 
@@ -29,25 +36,17 @@ export default class OtherContainer extends Component {
         console.log(obj)
         const itemsRef = window.firebase.database().ref('items')
         itemsRef.push(obj)
-        /*
-        SomeService.sendMessage(obj) 
-            .then((response) => {
-                console.log(response)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-
-        */
     }
 
     render() {
-
         return (
             <div className="page-container">
+                <PageSection background={'white'}>
+                <Suspense fallback={<Spinner />}>
+                    <SomeItems items={this.state.items} />
+                </Suspense>
+                </PageSection>
                 <PageSection>
-
-                    
                     <SomeForm onSubmitForm={this.handleSubmitForm} />
                 </PageSection>
             </div>
