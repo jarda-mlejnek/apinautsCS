1906221d9fa33d8e60e0c7901a83966ddba7141f
Milan Brich <milan.brich@centrum.cz>
2018-12-06 14:57:55 +0100

    meeting container - added id

--- src/containers/MeetingContainer.js
+++ src/containers/MeetingContainer.js
@@ -1,26 +1,38 @@
 import React, {Component} from 'react'
 import PageSection from '../components/shared/PageSection'
 import {CONFIG} from "../config";
-import PersonList from "../components/other/PersonList";
+import Person from "../components/other/Person";
 import {Link} from "react-router-dom";
 import Button from "../components/shared/forms/Button";
 import {QRCodeService} from '../services/QRCodeService'
+import * as queryString from "query-string";
 
 export default class MeetingContainer extends Component {
 
-	constructor(props) {
-		super(props)
-        this.state.uuid = QRCodeService.create_UUID()
+    state = {
+        people: [],
+    }
+
+    constructor(props) {
+        super(props)
+        this.state.uuid = this.getUuid()
         this.state.qrUrl = QRCodeService.getLoginPageQRCode(this.state.uuid)
         this.state.fireBaseSchema = CONFIG.FIREBASE_SCHEMAS.ITEMS + '-' + this.state.uuid
-	}
-
-    state = {
-    	people: [],
-        qrUrl: '',
-        uuid: '',
-        fireBaseSchema: ''
     }
+
+    getUuid() {
+        const queryParams = queryString.parse(this.props.location.search)
+
+        if (queryParams.id) {
+            console.log('obtained id: ', queryParams.id)
+            return queryParams.id
+        } else {
+            let uuid = QRCodeService.createUuid();
+            console.log('generated uuid: ', uuid)
+            return uuid
+        }
+    }
+
 
     componentDidMount() {
         const itemsRef = window.firebase.database().ref(this.state.fireBaseSchema)
@@ -39,58 +51,28 @@ export default class MeetingContainer extends Component {
                     mdRate: items[item].mdRate
                 })
             }
-            console.log('result.name: ' + result[result.length-1].name + ' result.mdRate: ' + result[result.length-1].mdRate
-                + ' result.id: ' + result[result.length-1].id)
             this.setState({people: result})
-
         })
     }
 
     render() {
-
-        const people = [
-            {
-                name: "666",
-                mdRate: "dsad"
-            }, 
-            {
-                name: "666",
-                mdRate: "dsad"
-            }, 
-            {
-                name: "666",
-                mdRate: "dsad"
-            }, 
-            {
-                name: "666",
-                mdRate: "dsad"
-            }, 
-
-        ]
-
-    	return (
-    		<div className="page-container">
-    			<PageSection background={'blue'}>
-                    <div className="meeting-grid">
-                        <div className="header">
-                            <div className="qr-header">
-                                <h5>Join us</h5>
-                            </div>
-                        </div>
-                        <div className="qr-code-container">
-                            <div className="qr-image">
-                                <img src={this.state.qrUrl} className="qr-code" />
-                            </div>
-                            <div className="register-button-container">
-                                <Link to="/app/summary">
-                                    <Button type="submit" name="button" class="green big" label="Start" />
-                                </Link>
-                            </div>
-                        </div>
-                        <PersonList items={this.state.people} />
+        return (
+            <div className="page-container">
+                <PageSection background={'blue'}>
+                    <div>
+                        <h5>Join us</h5>
+                        <img src={this.state.qrUrl}/>
                     </div>
-    			</PageSection>
-    		</div>
-    	)     
+                    <div>
+                        <Link to="/app/summary">
+                            <Button type="submit" name="button" class="green" label="Start"/>
+                        </Link>
+                    </div>
+                    <div className="meeting-form-section-grid">
+                        <Person items={this.state.people}/>
+                    </div>
+                </PageSection>
+            </div>
+        )
     }
 }
